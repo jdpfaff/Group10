4c90e17e4aeed2b63efe6e05e8edb04da3442556
Ryan Pfaff <jdpfaff@hotmail.com>
2020-02-13 07:40:49 -0500

    Initialized code folder and added source code for the Main Menu

---
+++ code/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        // The main menu is under Scene 0 and the actual game is under scene 1
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        // Debug to check in Unity editor
+        Debug.Log("Quit game");
+        Application.Quit();
+    }
+}
