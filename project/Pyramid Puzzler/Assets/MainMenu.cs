095355e3c5cd328e2e4a70f0754ce7d8f8d94eeb
Ryan Pfaff <jdpfaff@hotmail.com>
2020-02-14 17:00:32 -0500

    Added main menu

---
+++ project/Pyramid Puzzler/Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        // The main menu is under Scene 0 and the actual game is under scene 1
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        // This is only used when in the Unity Editor, or else it wouldn't close there either
+        EditorApplication.isPlaying = false;
+    }
+}
